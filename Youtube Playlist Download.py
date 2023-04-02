98aeb7de642f377e0a6230307df94dc3424493f7
anup.mirkute <anupmirkute1008@gmail.com>
2023-04-02 15:13:01 +0530

    Revert "Code Efficient in Renaming function"
    This reverts commit 5fd5dcf17e7a557c9ff802792fa25cbdeb75aaa4.

--- Youtube Playlist Download.py
+++ Youtube Playlist Download.py
@@ -20,12 +20,11 @@ def renameingFile(SAVE_PATH, title, index):
     """
     print("Renaming...")
 
-    # avoid_symbols = ['\\', '/', ':', '*', '?', '"', '<', '>', '|', '\'', '.', ',']
-    # for symbol in avoid_symbols:
-    #    for char in title:
-     #       if symbol == char :
-      #          title = title.replace(char, '')
-      title = ''.join(char for char in title if char not in avoid_symbols)
+    avoid_symbols = ['\\', '/', ':', '*', '?', '"', '<', '>', '|', '\'', '.', ',']
+    for symbol in avoid_symbols:
+        for char in title:
+            if symbol == char :
+                title = title.replace(char, '')            
                 
     old_name = SAVE_PATH + r"\{}.mp4".format(title)
     new_name = SAVE_PATH + r"\[ -- {} -- ] {}.mp4".format(str(index), title)
